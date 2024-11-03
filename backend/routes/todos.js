83ea1176cecff17cd877e8cd674b41e6bd81c644
chaapatti <137924830+chaapatti@users.noreply.github.com>
2024-11-03 18:59:16 +0000

    get todos

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,9 +1,19 @@
 const express = require("express");
-
+import SigninCheck from "../userMiddleware";
+const { Todo, User } = require("../db")
 const router = express.Router();
 // SO the todos are for the user so before reaching here we need to check if the user is signed in already.
-router.get("/alltodos", (req, res) => {
-    res.send("here are all the todos.")
+router.get("/alltodos",SigninCheck, (req, res) => {
+    const userId = res.locals.userId;
+    const user = User.findOne({
+        _id: userId
+    })
+    if (user){
+
+       return res.json({
+        Todos: user.todos
+       })
+    }
     
 })
 module.exports = router
