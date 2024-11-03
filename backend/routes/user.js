7cd80bba0cd0448e9cba4131c1651afd21f863c4
chaapatti <137924830+chaapatti@users.noreply.github.com>
2024-11-03 16:06:44 +0000

    added zod validation

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const { User } = require('../db');
-
+const jwt = require("jsonwebtoken")
+const z = require("zod")
 const router = express.Router();
 
 router.get("/", (req, res)=> {
     res.send("Home page user");
 })
 
+const signupInput = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+    firstName: z.string(),
+    lastName: z.string(),
+
+})
+
 router.post("/signup", async (req, res)=> {
     const body = req.body;
+    const { success } = z.safeParse(signupInput);
+    if (!success) {
+        res.status(403);
+        return res.json({
+            message:"Invalid user credential. Please check the information."
+        })
+    }
+    // Need some zod validation and password hashing.
     const existingUser = await User.findOne({
-        email: req.body.email
+        email: body.email
     })
     if (existingUser) {
         res.status(403);
@@ -24,11 +41,56 @@ router.post("/signup", async (req, res)=> {
         firstName: body.firstName,
         lastName: body.lastName
     })
+
+    if(user) {
+        const token = jwt.sign({userId: user._id}, "MY_VEERY_SECRET_KEY")
+        localStorage.setItem("token", token);
+        return res.json({
+            jwt: token
+        })
+    }
+    else{
+        res.status(411);
+        res.json({
+            message: "Unauthorized access"
+        })
+    }
     
 })
 
-router.post("/signin", (req, res) => {
-    res.send("welcome to signinpage.")
+const signinInput = z.object({
+    email: z.string().email(),
+    password: z.string()
+})
+
+
+router.post("/signin", async (req, res) => {
+    const body = req.body;
+    // Need zod validation here also
+    const { success } = z.safeParse(signinInput);
+    if (!success) {
+        res.status(403);
+        return res.json({
+            message:"Invalid user credential. Please check the information."
+        })
+    }
+    const user = await User.findOne({
+        email: body.email,
+        password: body.password
+    })
+    if (user) {
+        const token = jwt.sign({userId: user._id}, "MY_VEERY_SECRET_KEY")
+        localStorage.setItem("token", token)
+        return res.json({
+            jwt: token
+        })
+    }
+    else{
+        res.status(411);
+        return res.json({
+            message: "Incorrect email or password."
+        })
+    }
 })
 
 module.exports = router
